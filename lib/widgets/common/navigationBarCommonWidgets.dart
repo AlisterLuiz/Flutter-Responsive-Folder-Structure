e0a5dea2a754f2fad32b2854849830e85f162d09
Alister Luiz <alisterluiz@gmail.com>
2020-07-06 17:58:02 +0400

    Responsive Layout updated

---
+++ lib/widgets/common/navigationBarCommonWidgets.dart
@@ -0,0 +1,8 @@
+import 'package:Flutter_Responsive_Folder_Structure/utilities/index.dart';
+
+class NavigationBarCommonWidgets extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
